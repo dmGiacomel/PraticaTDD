3d90144acd7189b58feab46a1daca5550d3ac858
dmGiacomel <davimarchettigiacomel1509@gmail.com>
2023-06-27 14:25:30 -0300

    terceiro commit

--- Money.java
+++ Money.java
@@ -2,13 +2,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-class Money  {
-   protected int amount;
+abstract class Money  {
+    protected int amount;
    
-   public boolean equals(Object object)  {
-      Money money = (Money) object;
-      return amount == money.amount;
-   }   
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
+    } 
+
+    static Dollar dollar(int amount)  {
+        return new Dollar(amount);
+    }
+	
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);  
+
 }
 
 
@@ -17,4 +28,11 @@ public void testEquality() {
    assertFalse(new Dollar(5).equals(new Dollar(6)));
    assertTrue(new Franc(5).equals(new Franc(5)));
    assertFalse(new Franc(5).equals(new Franc(6)));
+   assertFalse(new Franc(5).equals(new Dollar(5)));
+}
+
+public void testMultiplication() {
+   Money five = Money.dollar(5);
+   assertEquals(Money.dollar(10), five.times(2));
+   assertEquals(Money.dollar(15), five.times(3));
 }
